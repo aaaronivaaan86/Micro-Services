cf07c2fea4138e059def85e2e567cd75f7312c4e
Brian Magana A <brian.magana@voxelmaps.com>
2022-11-14 21:06:28 -0600

    Add Respositories and Services

---
+++ PostalCodeService/Configuration/PostalCodeStoreDatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace PostalCodeService.Configuration
+{
+    public class PostalCodeStoreDatabase
+    {
+
+        /// <summary>Gets or sets the connection string.</summary>
+        /// <value>The connection string.</value>
+        public string ConnectionString { get; set; }
+
+        /// <summary>Gets or sets the name of the database.</summary>
+        /// <value>The name of the database.</value>
+        public string DatabaseName { get; set; }
+
+        /// <summary>Gets or sets the name of the books collection.</summary>
+        /// <value>The name of the books collection.</value>
+        public string PostalCodeCollection { get; set; } 
+    }
+}
